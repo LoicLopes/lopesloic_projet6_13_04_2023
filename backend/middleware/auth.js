ad36e93bc1857596af2b26e4ace386857c7ca61d
Moi <lopesloic64@gmail.com>
2023-05-18 10:16:35 +0200

    Create structure middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,17 @@
-const jsonwebtoken = require('jsonwebtoken');
+const jsontoken = require('jsonwebtoken');
 
 module.exports = (req, res, next)=>{
     try{
+        console.log('authentification 1');
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jsonwebtoken.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jsontoken.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
+        console.log('authentification 2');
         req.auth = {
             userId: userId
         };
     }catch(error){
+        console.log('authentification du token erreur');
         res.status(401).json({ error });
     }
-    
 };
